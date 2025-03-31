7dcd35a2835790faefcf768f1252925bf550f419
Sachin9548 <138348581+Sachin9548@users.noreply.github.com>
2025-03-31 15:51:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 // static file serving from dist folder from the client
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, './client/dist/index.html'));
 });
 
 
