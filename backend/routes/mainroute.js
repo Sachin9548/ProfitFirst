2c594d215687e7c9deee71e0d2afc8bbf3d15564
Sachin9548 <138348581+Sachin9548@users.noreply.github.com>
2025-03-31 15:44:39 +0530

    Delete backend directory

--- backend/routes/mainroute.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const getInTouchController = require('../controller/getInTouchController');
-const router = express.Router();
-router.post('/getInTouch',getInTouchController  );
-module.exports = router;
